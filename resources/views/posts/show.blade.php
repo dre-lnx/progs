bf850f824252ccf55bf5accf5692ea6694f703d3
dre-lnxd <andrevlds23@gmail.com>
2022-01-17 19:14:12 -0300

    Entire post layout adjust

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,7 +2,18 @@
 @section('title', 'Visualizar post inteiro')
 
 @section('content')
-<h1>Titulo: {{ $post->titulo }}</h1>
-<img src="{{asset('img/' . $post->imagem)}}">
-<p>Descrição: {{$post->descricao}}</p>
+<div class="row"></div>
+    <div class="col">
+        <div class="row post-topic">
+            <h1>{{ $post->titulo }}</h1>
+        </div>
+        <div class="row post-topic">
+            <img src="{{asset('img/' . $post->imagem)}}" class="post-img">
+        </div>
+        <div class="row post-topic">
+            <p>{{$post->descricao}}</p>
+        </div>
+        
+    </div>
+</div>
 @endsection
--- resources/views/templates/base.blade.php
+++ resources/views/templates/base.blade.php
@@ -28,6 +28,15 @@
         cursor: pointer;
         background-color: #d6d6d6;
       }
+
+      .post-topic {
+        padding-top: 20px;
+      }
+
+      .post-topic .post-img {
+        max-width: 600px;
+      }
+
     </style>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
